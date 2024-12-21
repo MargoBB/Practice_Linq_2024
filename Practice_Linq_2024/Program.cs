c65456eaeb7a844f622c562a2f17d8ebd8627c12
Margo Berlin <margoberlin93@gmail.com>
2024-12-21 02:20:52 +0200

    QUERY 4

--- Practice_Linq_2024/Program.cs
+++ Practice_Linq_2024/Program.cs
@@ -90,18 +90,17 @@ namespace Practice_Linq_2024
         // Запит 4
         static void Query4(List<FootballGame> games)
         {
-            //Query 4: Вивести всі матчі збірної Германії з 2018 року по 2020 рік (включно), в яких вона на виїзді програла.
+            var selectedGames = games
+                .Where(g => g.Away_team == "Germany" && g.Date.Year >= 2018 && g.Date.Year <= 2020 && g.Home_score > g.Away_score)
+                .ToList();
 
-            var selectedGames = games;   // Корегуємо запит !!!
-
-
-            // Перевірка
             Console.WriteLine("\n======================== QUERY 4 ========================");
-
-            // див. приклад як має бути виведено:
-
-
+            foreach (var game in selectedGames)
+            {
+                Console.WriteLine($"{game.Date:dd.MM.yyyy} {game.Home_team} - {game.Away_team}, Score: {game.Home_score} - {game.Away_score}, Country: {game.Country}");
+            }
         }
+
 
         // Запит 5
         static void Query5(List<FootballGame> games)
